f6cf0f2978f98b081d036b218bdcfd0a33ab9520
Reid McLendon <rmcle0010@mail.ct.edu>
2017-10-20 22:25:03 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import React from 'react';
-impoprt ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+
+var meInReact = React.createElement(
+  <div class="me">
+  <h1>An Awesome Person</h1>
+  <p>Who is learning React</p>
+ 
+  <ul class="my-interests">
+    <li>JavaScript</li>
+    <li>React</li>
+    <li>Movies</li>
+    <li>Ice cream</li>
+  </ul>
+</div>
+)
